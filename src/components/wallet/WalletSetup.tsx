453c7debeb36d440dd3652c8bc694f59caf9d4c6
Santatriniaina Tiantsoa <miaritratsiketa@gmail.com>
2025-09-09 12:48:58 +0300

    Port

--- src/components/wallet/WalletSetup.tsx
+++ src/components/wallet/WalletSetup.tsx
@@ -18,7 +18,7 @@ const WalletSetup = ({ onWalletCreated }: { onWalletCreated: (walletData: any) =
   const createWallet = async () => {
     setLoading(true);
     try {
-      const response = await fetch("http://localhost:3000/wallet/create", {
+      const response = await fetch("http://localhost:5002/wallet/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
